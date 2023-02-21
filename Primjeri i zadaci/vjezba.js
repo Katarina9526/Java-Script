c346eb9d5701ef579e6f33d871597fa81889b1e1
katarina <katarinajurkovic95@gmail.com>
2023-02-21 18:14:47 +0100

    unesene promjene

--- Primjeri i zadaci/vjezba.js
+++ Primjeri i zadaci/vjezba.js
@@ -1,2 +1,16 @@
+var names = ["John", "Jane", "Bob", , "Mike"];
+names.push("Katarina")
+
+for (var i = 0; i < names.length; i++){
+    //console.log(names[i]);
+}
+
+var filtered = names.filter(function(x) {
+    return x !== undefined;
+});
+
+console.log(filtered);
+
+
 
 
