ad0328125b357c06528afbfa9c688da6e7146382
katarina <katarinajurkovic95@gmail.com>
2023-02-21 18:23:03 +0100

    dovršen zadatak

--- Primjeri i zadaci/vjezba.js
+++ Primjeri i zadaci/vjezba.js
@@ -1,15 +1,18 @@
 var names = ["John", "Jane", "Bob", , "Mike"];
 names.push("Katarina")
+//let text = ""
 
-for (var i = 0; i < names.length; i++){
-    //console.log(names[i]);
+for (var i = 0; i < names.length; i++) {
+    /*if (i === 2) {break;}
+    text += "The name is" + i + "<br>";*/
+    console.log(names[i]);
 }
 
 var filtered = names.filter(function(x) {
     return x !== undefined;
 });
 
-console.log(filtered);
+//console.log(filtered);
 
 
 
