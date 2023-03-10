515c48253c51c51360f2c947be46606aa8f19e58
katarina <katarinajurkovic95@gmail.com>
2023-03-10 17:19:29 +0100

    dovršena vježba 4

---
+++ Primjeri i zadaci/vjezba4.js
@@ -0,0 +1,34 @@
+var character = {
+    name: 'Eleven',
+    show: 'Stranger Things',
+    portrayedBy: 'Millie Bobby Brown'
+  }
+  
+  const quotes = [
+      'I\'m going to my friends. I\'m going home.', 
+      'Pure fuel! PURE FUEL! WOO!', 
+      'See? Zoomer.', 
+      'Bitchin.'
+  ];
+  
+  //1)
+  var proto1 = Object.getPrototypeOf(character);
+  var proto2 = Object.getPrototypeOf(proto1);
+
+  console.log(proto1);
+  console.log(proto2);
+
+  var rndCnt = parseInt(Math.random()*10) % 4;
+  console.log(rndCnt);
+  var rndVal = quotes[rndCnt];
+  console.log(rndVal);
+
+  character.prototype = function(){
+    //console.log(rndCnt);
+    let rndVal = quotes[rndCnt];
+    //console.log(rndVal);
+    return rndVal;
+  }
+
+  console.log(character);
+  //console.log(proto1);
