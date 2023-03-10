1581d3e0280544458f7f1140c1d9e937d381b5f2
katarina <katarinajurkovic95@gmail.com>
2023-03-10 19:05:55 +0100

    dovršena vježba 5

--- Primjeri i zadaci/vjezba5.js
+++ Primjeri i zadaci/vjezba5.js
@@ -1,6 +1,15 @@
 function Dog(name) {
-    this.name = name || "Rex";
+    this.name = name ? name : "Rex";
+    this.getName = function() {
+        return this.name;
+    }
 }
 
 var dog1 = new Dog();
 console.log(dog1.name);
+
+var dog2 = new Dog("Floki");
+console.log(dog2.name);
+
+var dog3 = new Dog("Fifi");
+console.log(dog3.getName());
